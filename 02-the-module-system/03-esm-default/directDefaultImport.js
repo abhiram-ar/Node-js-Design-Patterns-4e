ee114dfd742c9353ff4ff36c188fe99e1e3e2eb2
abhiram-ar <abhiram2001.ars@gmail.com>
2025-10-30 00:28:22 +0530

    default exports

--- 02-the-module-system/03-esm-default/directDefaultImport.js
+++ 02-the-module-system/03-esm-default/directDefaultImport.js
@@ -1,5 +1,10 @@
 // Uncomment and execute the code below to trigger a "SyntaxError: Unexpected reserved word"
 
-// import { default } from './logger.js'
-// const logger = new default('a')
-// logger.log('Hello World')
+
+import { default } from './logger.js'
+const logger = new default('a')
+logger.log('Hello World')
+
+// mynote: we cannot use the variable default as it is a reserved word in the language
+// but  import * as something from "somewhere"
+// something.default is valid
--- 02-the-module-system/03-esm-default/showDefault.js
+++ 02-the-module-system/03-esm-default/showDefault.js
@@ -1,4 +1,5 @@
 // biome-ignore lint/style/noNamespaceImport: needs namespace import to show the default export
-import * as loggerModule from './logger.js'
+import * as loggerModule from "./logger.js";
 
-console.log(loggerModule.default)
+console.log(loggerModule);
+console.log(loggerModule.default);
